941c1ae128fb06801246db7029ee351e94ed2419
erictmay@me.com <erictmay@me.com>
2019-01-26 08:11:36 -0500

    calculator that sucks

---
+++ app/controllers/calculate_controller.rb
@@ -0,0 +1,15 @@
+class CalculateController < ApplicationController
+
+  def calculate
+
+    if params[:addition]
+      @result = params[:first_number].to_f + params[:second_number].to_f
+    end
+
+    if params[:subtraction]
+      @result = params[:first_number].to_f - params[:second_number].to_f
+    end
+
+  end
+end
+t
